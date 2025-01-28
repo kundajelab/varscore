dad7605d532716a693f6772f3b11d2c5cb82670b
Riya Sinha <riya.v.sinha@gmail.com>
2025-01-28 00:37:04 +0000

    add prioritization endpoint

--- service/server.py
+++ service/server.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -25,10 +26,10 @@ app.add_middleware(
 async def annotate(input: VariantAnnotationInput) -> AnnotatedVariant:
     return annotate_variant(input)
 
-# @app.post("/prioritize/")
-# async def predict(input_data: dict):
-#     df = pd.DataFrame(input_data)
-#     return prioritize_variants(df).to_dict(orient="records")
+@app.post("/prioritize/")
+async def predict(input_data: List[dict]) -> List[dict]:
+    df = pd.DataFrame(input_data)
+    return prioritize_variants(df).to_dict(orient="records")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
